66061748825bab54b229d2d6857e9cb96f76e6f9
cbarrios <lalobarrios16@yahoo.com>
2022-05-06 12:21:01 -0400

    master: (5) Memory in a composable function

--- app/src/main/java/com/lalosapps/state/WaterCounter.kt
+++ app/src/main/java/com/lalosapps/state/WaterCounter.kt
@@ -5,6 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,7 +21,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        // Changes to count are now tracked by Compose
+        var count by remember { mutableStateOf(0) }
         Text(text = "You've had $count glasses.")
         Button(onClick = { count++ }, modifier = modifier.padding(top = 8.dp)) {
             Text(text = "Add one")
